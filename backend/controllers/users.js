b33f74c0623bfe21d20003f4f7c50b5a1980a65a
Josef Hedblom <hedblomjosef@gmail.com>
2021-09-07 16:07:02 +0200

    Basic backend config.

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,1 +1,21 @@
-// Users controller to create, update, delete, login users
+
+exports.member = (req, res) => {
+  // Find one user
+}
+
+exports.signup = (req, res) => {
+  // Create new user
+};
+
+exports.login = (req, res) => {
+  // Login user
+};
+
+exports.update = (req, res) => {
+  // Update user
+};
+
+exports.delete = (req, res) => {
+  // Delete user
+}
+
