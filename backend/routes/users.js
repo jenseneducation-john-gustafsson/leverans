d58a3d6a546a3223d66659a51feaf6c77f63ff29
Josef Hedblom <hedblomjosef@gmail.com>
2021-09-07 16:07:02 +0200

    Basic backend config.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,1 +1,8 @@
-// Users routes to handel incoming request from frontend to send to the user controller
+const router = require("express").Router();
+const UserController = require('../controllers/users');
+
+router.get("/members/:username", UserController.member);
+router.post("/signup", UserController.signup);
+router.post("/login", UserController.login);
+
+module.exports = router;
