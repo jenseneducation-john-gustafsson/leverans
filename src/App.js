eac648d6ad12d0f924a9f8921769ea17256c00a5
jenseneducation-john-gustafsson <john.gustafsson80@gmail.com>
2021-09-07 15:00:14 +0200

    lagt till header och navbar component filer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import {Header} from "./components/common"
 
 function App() {
   return (
     <div className="App">
-      <h1>Notflix</h1>
+      <Header/>
     </div>
   );
 }
