59953b1730258553992cd5438932227ff29fcf0e
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-09-07 17:27:10 +0200

    Trailer link + icon component

---
+++ src/components/MovieDetails/Trailer.js
@@ -0,0 +1,13 @@
+import { FaPlay } from 'react-icons/fa'
+
+const Trailer = () => {
+  return (
+    <div className="trailer">
+      <FaPlay />
+      <a href="#">Trailer</a>
+
+    </div>
+  )
+}
+
+export default Trailer
