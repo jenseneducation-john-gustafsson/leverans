a0f504fdb0133cfff6c27bec25b3d7b5082f5630
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-09-08 13:02:23 +0200

    Rating component

---
+++ src/components/MovieDetails/Rating.js
@@ -0,0 +1,14 @@
+import { FaRegStar } from 'react-icons/fa'
+
+const Rating = () => {
+  return (
+    <div className="rating">
+      <h6>Rating</h6>
+      <FaRegStar />
+      <FaRegStar />
+      <FaRegStar />
+    </div>
+  )
+}
+
+export default Rating
