3ace479ea8f184d5ce60743aff00c4d21b2381d9
jenseneducation-john-gustafsson <john.gustafsson80@gmail.com>
2021-09-08 14:19:18 +0200

    navbar design

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div>
+      <div className="nav">
+        <p className="nav-text">Top Rated</p>
+        <p className="nav-text">Latest</p>
+        <p className="nav-text">Bundles</p>
+        <p className="nav-text">Genre</p>
+      </div>
+    </div>
+  )
+}
+export default Navbar
