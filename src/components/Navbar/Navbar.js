bedcf9cd033ed9a5f9e37615a9f0c1dc38d11227
jenseneducation-john-gustafsson <john.gustafsson80@gmail.com>
2021-09-08 14:24:37 +0200

    gjort navbar klickbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,11 +3,11 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <div>
-      <div className="nav">
-        <p className="nav-text">Top Rated</p>
-        <p className="nav-text">Latest</p>
-        <p className="nav-text">Bundles</p>
-        <p className="nav-text">Genre</p>
+      <div className="navbar">
+        <button className="navbar-buttons"><p className="nav-text">Top Rated</p></button>
+        <button className="navbar-buttons"><p className="nav-text">Latest</p></button>
+        <button className="navbar-buttons"><p className="nav-text">Bundles</p></button>
+        <button className="navbar-buttons"><p className="nav-text">Genre</p></button>
       </div>
     </div>
   )
