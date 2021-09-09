776d7713d1687933637ab204b05a0c589ea03680
jenseneducation-john-gustafsson <john.gustafsson80@gmail.com>
2021-09-09 21:49:10 +0200

    lagt till startsidan med lite text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./components/Header/Header"
 import Navbar from "./components/Navbar/Navbar"
+import Startpage from "./view/startpage"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
+      <Startpage />
     </div>
   );
 }
---
+++ src/view/startpage.js
@@ -0,0 +1,16 @@
+import "./startpage.css"
+
+function startpage() {
+  return (
+    <div>
+      <h2 className="title">Welcome to our site</h2>
+      <p>Watching the films you love right from the comfort of your own home.
+        Here you will find all the best movies from 5 different genres, Documentary, Rom-Com, Horror, Film-noir and Action.
+        If you click on Top Rated you will find our favorite picks at Notflix.</p>
+      <br></br>
+      <p></p>
+    </div>
+  )
+}
+
+export default startpage
