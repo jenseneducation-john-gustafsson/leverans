ff22757604dd7f60c5f907491c0e68e16eb46bdc
Romsep <chilescote1@hotmail.com>
2021-09-11 01:20:42 +0200

    Bundle 1.1

--- src/Bundle.js
+++ src/Bundle.js
@@ -1,5 +1,41 @@
-import './bundle.css';
-import pic from '../src/components/Bundle/Pictures/joker.png'
+import '../src/components/Bundle/bundle.css';
+
+//import pic from '../src/components/Bundle/Pictures/joker.png'
+//Horror
+import Scream from '../src/components/Bundle/Pictures/Scream.jpg'
+import TheCounjuring from '../src/components/Bundle/Pictures/TheConjuring.png'
+import ThePurge from '../src/components/Bundle/Pictures/ThePurge.jpg'
+import TheRing from '../src/components/Bundle/Pictures/TheRing.jpg'
+//Action
+import DC from '../src/components/Bundle/Pictures/DC.jpg'
+import BadBoys from '../src/components/Bundle/Pictures/BadBoys.jpg'
+import TheLordOfTheRings from '../src/components/Bundle/Pictures/TheLordOfTheRings.jpg'
+import XMen from '../src/components/Bundle/Pictures/X-Men.jpg'
+//Comedy
+import ScaryMovie from '../src/components/Bundle/Pictures/ScaryMovie.jpg'
+import MeanGirls from '../src/components/Bundle/Pictures/MeanGirls.jpg'
+import HotShot from '../src/components/Bundle/Pictures/HotShot.jpg'
+import Big from '../src/components/Bundle/Pictures/Big.jpg'
+//Drama
+import WW2 from '../src/components/Bundle/Pictures/WW2.jpg'
+import TheGodfather from '../src/components/Bundle/Pictures/TheGodfather.jpg'
+import Rocky from '../src/components/Bundle/Pictures/Rocky.jpg'
+import Best from '../src/components/Bundle/Pictures/5Best.jpg'
+//Adventure
+import Jumanji from '../src/components/Bundle/Pictures/Jumanji.jpg'
+import MIB from '../src/components/Bundle/Pictures/MIB.jpg'
+import JurassicWorld from '../src/components/Bundle/Pictures/JurassicWorld.jpg'
+import TombRaider from '../src/components/Bundle/Pictures/TombRaider.jpg'
+//Sci-Fi
+import StarTrek from '../src/components/Bundle/Pictures/StarTrek.jpg'
+import StarTrekTripple from '../src/components/Bundle/Pictures/StarTrekTripple.jpg'
+import Sci from '../src/components/Bundle/Pictures/Sci-Films.jpg'
+import Independence from '../src/components/Bundle/Pictures/Independence.jpg'
+//Animation
+import Shrek from '../src/components/Bundle/Pictures/Shrek.jpg'
+import KungFuPanda from '../src/components/Bundle/Pictures/KungFuPanda.jpg'
+import Minions from '../src/components/Bundle/Pictures/Minions.jpg'
+import Madagascar from '../src/components/Bundle/Pictures/Madagascar.jpg'
 
 function App() {
   return (
@@ -14,36 +50,33 @@ function App() {
 
         <div class="column">
           <div class="content">
-          <img src={pic} alt="joker" width ="196px" height="288px"/>
-            <h3>movie</h3>
-            <p>Lorem ipsum..</p>
+          <img src={Scream} alt="Scream" width ="196px" height="288px"/>
+            <h3>Scream Collection</h3>
+            <p>Save 88.00 kr when you buy this bundle rather than each movie individually.</p>
             <button>Buy</button>
           </div>
         </div>
-
         <div class="column">
           <div class="content">
-          <img src={pic} alt="joker" width ="196px" height="288px"/>
-            <h3>movie</h3>
-            <p>Lorem ipsum..</p>
+          <img src={TheCounjuring} alt="TheCounjuring" width ="196px" height="288px"/>
+            <h3>Conjuring Universe </h3>
+            <p>Save 264.00 kr when you buy this bundle rather than each movie individually.</p>
             <button>Buy</button>
           </div>
         </div>
-
         <div class="column">
           <div class="content">
-      <img src={pic} alt="joker" width ="196px" height="288px"/>
-            <h3>movie</h3>
-            <p>Lorem ipsum..</p>
+      <img src={ThePurge} alt="ThePurge" width ="196px" height="288px"/>
+            <h3>The Purge Collection</h3>
+            <p>Save 67.00 kr when you buy this bundle rather than each movie individually.</p>
             <button>Buy</button>
           </div>
         </div>
-
         <div class="column">
           <div class="content">
-         <img src={pic} alt="joker" width ="196px" height="288px"/>
-            <h3>movie</h3>
-            <p>Lorem ipsum..</p>
+         <img src={TheRing} alt="TheRing" width ="196px" height="288px"/>
+            <h3>The Rings 3-Movie Collection</h3>
+            <p>Save 78.00 kr when you buy this bundle rather than each movie individually.</p>
             <button>Buy</button>
           </div>
         </div>
@@ -54,33 +87,33 @@ function App() {
 
           <div class="column">
             <div class="content">
-           <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+           <img src={DC} alt="DC" width ="196px" height="288px"/>
+              <h3>DC 7-Film Collection</h3>
+              <p>Save 194.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={BadBoys} alt="BadBoys" width ="196px" height="288px"/>
+              <h3>Bad Boys 3-Movie Collection</h3>
+              <p>Save 88.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={TheLordOfTheRings} alt="TheLordOfTheRings" width ="196px" height="288px"/>
+              <h3>The Lord of the Rings Collection</h3>
+              <p>Save 98.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={XMen} alt="XMen" width ="196px" height="288px"/>
+              <h3>X-Men Trilogy: Extended Bundle</h3>
+              <p>Save 48.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
@@ -91,32 +124,33 @@ function App() {
           <div class="row2">
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={ScaryMovie} alt="ScaryMovie" width ="196px" height="288px"/>
+              <h3>Scary Movie Collection</h3>
+              <p>Save 88.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={MeanGirls} alt="MeanGirls" width ="196px" height="288px"/>
+              <h3>Mean Girls Collection</h3>
+              <p>Save 40.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={HotShot} alt="HotShot" width ="196px" height="288px"/>
+              <h3>Hot Shots  Collection</h3>
+              <p>Save 19.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
-          </div> <div class="column">
+          </div>
+          <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={Big} alt="Big" width ="196px" height="288px"/>
+              <h3>Night at the Museum</h3>
+              <p>Save 68.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
@@ -130,32 +164,33 @@ function App() {
           <div class="row2">
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={WW2} alt="WW2" width ="196px" height="288px"/>
+              <h3>WWII Films 5 Movies</h3>
+              <p>Save 146.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={TheGodfather} alt="TheGodfather" width ="196px" height="288px"/>
+              <h3>The Godfather Trilogy</h3>
+              <p>Save 38.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={Rocky} alt="Rocky" width ="196px" height="288px"/>
+              <h3>Rocky Heavyweight </h3>
+              <p>Save 245.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
-          </div> <div class="column">
+          </div> 
+          <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={Best} alt="5Best" width ="196px" height="288px"/>
+              <h3>Best Picture 5-Film Collection</h3>
+              <p>Save 126.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
@@ -168,32 +203,33 @@ function App() {
           <div class="row2">
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={Jumanji} alt="Jumanji" width ="196px" height="288px"/>
+              <h3>Welcome to the Jumanji</h3>
+              <p>Save 9.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={MIB} alt="MIB" width ="196px" height="288px"/>
+              <h3>Men in Black 4 Movie Collection</h3>
+              <p>Save 117.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={JurassicWorld} alt="JurassicWorld" width ="196px" height="288px"/>
+              <h3>Jurassic World Collection</h3>
+              <p>Save 96.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
-          </div> <div class="column">
+          </div>
+          <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={TombRaider} alt="TombRaider" width ="196px" height="288px"/>
+              <h3>Tomb Raider Movie Collection</h3>
+              <p>Save 20.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
@@ -206,32 +242,33 @@ function App() {
           <div class="row2">
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={StarTrek} alt="StarTrek" width ="196px" height="288px"/>
+              <h3>Star Trek The Original</h3>
+              <p>Save 155.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={StarTrekTripple} alt="StarTrekTripple" width ="196px" height="288px"/>
+              <h3>Star Trek 3 Film Collection</h3>
+              <p>Save 78.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={Sci} alt="Sci" width ="196px" height="288px"/>
+              <h3>Sci-Fi 5 Film Collection</h3>
+              <p>Save 86.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
-          </div> <div class="column">
+          </div>
+          <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={Independence} alt="Independence" width ="196px" height="288px"/>
+              <h3>Independence Day: Collection</h3>
+              <p>Save 19.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
@@ -244,32 +281,33 @@ function App() {
           <div class="row2">
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={Shrek} alt="Shrek" width ="196px" height="288px"/>
+              <h3>Shrek 4-Movie Collection</h3>
+              <p>Save 67.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={KungFuPanda} alt="KungFuPanda" width ="196px" height="288px"/>
+              <h3>Kung Fu Panda Movie Collection</h3>
+              <p>Save 58.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
           <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={Minions} alt="Minions" width ="196px" height="288px"/>
+              <h3>Despicable Me 1-3: Minions</h3>
+              <p>Save 67.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
-          </div> <div class="column">
+          </div> 
+          <div class="column">
             <div class="content">
-            <img src={pic} alt="joker" width ="196px" height="288px"/>
-              <h3>movie</h3>
-              <p>Lorem ipsum..</p>
+            <img src={Madagascar} alt="Madagascar" width ="196px" height="288px"/>
+              <h3>Madagascar 4-Movie Collection</h3>
+              <p>Save 67.00 kr when you buy this bundle rather than each movie individually.</p>
               <button>Buy</button>
             </div>
           </div>
