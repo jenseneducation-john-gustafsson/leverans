f57cd8990f5b67120268bb71d094a4f217c99bf9
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-09-10 16:01:08 +0200

    Resolving issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+<<<<<<< HEAD
 import Header from "./components/Header/Header"
 import Navbar from "./components/Navbar/Navbar"
+=======
+import {Header} from "./components/common"
+>>>>>>> lagt till header och navbar component filer
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <Header />
       <Navbar />
+=======
+      <Header/>
+>>>>>>> lagt till header och navbar component filer
     </div>
   );
 }
