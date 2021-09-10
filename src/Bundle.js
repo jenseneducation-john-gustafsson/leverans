295da9ca643bcb7652582a4401764b6e032e93a4
Romsep <chilescote1@hotmail.com>
2021-09-10 13:25:58 +0200

    Bundle 1.0

---
+++ src/Bundle.js
@@ -0,0 +1,288 @@
+import './bundle.css';
+import pic from '../src/components/Bundle/Pictures/joker.png'
+
+function App() {
+  return (
+    <div class="main">
+
+      <h2>Bundle Of movies</h2>
+      <p>Get the best movie bundles deals so that you can enjoy the movie to the fullest!</p>
+
+      <h5>Horror</h5>
+
+      <div class="row2">
+
+        <div class="column">
+          <div class="content">
+          <img src={pic} alt="joker" width ="196px" height="288px"/>
+            <h3>movie</h3>
+            <p>Lorem ipsum..</p>
+            <button>Buy</button>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="content">
+          <img src={pic} alt="joker" width ="196px" height="288px"/>
+            <h3>movie</h3>
+            <p>Lorem ipsum..</p>
+            <button>Buy</button>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="content">
+      <img src={pic} alt="joker" width ="196px" height="288px"/>
+            <h3>movie</h3>
+            <p>Lorem ipsum..</p>
+            <button>Buy</button>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="content">
+         <img src={pic} alt="joker" width ="196px" height="288px"/>
+            <h3>movie</h3>
+            <p>Lorem ipsum..</p>
+            <button>Buy</button>
+          </div>
+        </div>
+        
+        <h5>Action</h5>
+
+        <div class="row2">
+
+          <div class="column">
+            <div class="content">
+           <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+
+
+          <h5>Comedy</h5>
+
+          <div class="row2">
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div> <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+
+           
+            </div>
+
+
+            <h5>Drama</h5>
+            
+          <div class="row2">
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div> <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+
+           
+            </div>
+
+            <h5>Adventure</h5>
+            
+          <div class="row2">
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div> <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+
+           
+            </div>
+
+            <h5>Sci-Fi</h5>
+            
+          <div class="row2">
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div> <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+
+           
+            </div>
+
+            <h5>Animation</h5>
+            
+          <div class="row2">
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+          <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div> <div class="column">
+            <div class="content">
+            <img src={pic} alt="joker" width ="196px" height="288px"/>
+              <h3>movie</h3>
+              <p>Lorem ipsum..</p>
+              <button>Buy</button>
+            </div>
+          </div>
+
+           
+            </div>
+
+
+          </div>
+
+        </div>
+      </div>
+      );
+  }
+
+      export default App;
