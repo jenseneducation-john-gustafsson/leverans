ebbd88e00147de8fa3db6c90769b3153a5db3321
Romsep <chilescote1@hotmail.com>
2021-09-12 15:17:46 +0200

    1.2 test Slideshow

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+// import './App.css';
+import Bundle from './components/Bundle/Bundle.js'
 
 function App() {
   return (
     <div className="App">
       <h1>Notflix</h1>
+      <Bundle/>
     </div>
   );
 }
