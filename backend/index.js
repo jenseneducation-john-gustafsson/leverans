0cce3ea605d0eca22685c2dbd0b79cd1b91a549f
divcoder20 <danny_tesfazion@hotmail.com>
2021-09-14 00:03:32 +0200

    added mongoose connection to database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 const { PORT } = require('./config/environment_variables');
+
+const ConnectDB = require("./config/db.js")
+
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+ConnectDB();
 
 app.use('/', (req, res) => {
   res.send('Notflix');
