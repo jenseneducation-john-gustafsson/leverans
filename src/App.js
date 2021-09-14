53064bbecd08b436f027e6cce743514d1c4b1d43
Nexuz49 <58955897+Nexuz49@users.noreply.github.com>
2021-09-14 12:34:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from "./components/Header/Header"
 import Navbar from "./components/Navbar/Navbar"
 // import CategoryDetailsAction from './components/Category-page/Category-details/CategoryDetailsAction'; // test purpose
-import CategoryDetailsComedy from './components/Category-page/Category-details/CategoryDetailsComedy'; // test purpose
+// import CategoryDetailsComedy from './components/Category-page/Category-details/CategoryDetailsComedy'; // test purpose
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <Navbar />
-      <CategoryDetailsComedy />
+      
     </div>
   );
 }
