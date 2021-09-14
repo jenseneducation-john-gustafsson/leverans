39b0076f1adafdfc50a6f777504e8f098fc0f0a0
Nexuz49 <58955897+Nexuz49@users.noreply.github.com>
2021-09-14 19:21:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import Header from "./components/Header/Header"
 import Navbar from "./components/Navbar/Navbar"
 // import CategoryDetailsAction from './components/Category-page/Category-details/CategoryDetailsAction'; // test purpose
 // import CategoryDetailsComedy from './components/Category-page/Category-details/CategoryDetailsComedy'; // test purpose
-import CategoryDetailsHorror from './components/Category-page/Category-details/CategoryDetailsHorror'; // test purpose
+// import CategoryDetailsHorror from './components/Category-page/Category-details/CategoryDetailsHorror'; // test purpose
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
-      <CategoryDetailsHorror />
     </div>
   );
 }
