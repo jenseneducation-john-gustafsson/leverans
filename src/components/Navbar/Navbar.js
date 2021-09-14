b1ce3f2473dccd4ae6797de8e0eed77a9e0427a8
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-09-14 11:52:41 +0200

    Remove core routing from Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,10 @@
 import "./Navbar.css";
 
-import Wishlist from "../../view/Wishlist";
-import Bundle from "../Bundle/Bundle";
-import Genre from "../../view/CategoryOverview"
-import StartPage from "../../view/StartPage"
-
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Navbar() {
 
   return (
-    <Router>
       <div>
         <nav>
           <div className="navbar">
@@ -21,22 +15,8 @@ function Navbar() {
             <Link to="/wishlist"><button className="navbar-buttons"><p className="nav-text">Wishlist</p></button></Link>
           </div>
         </nav>
-
-    <Route path="/" exact>
-      <StartPage />
-    </Route>
-    <Route path="/bundles">
-      <Bundle />
-    </Route>
-    <Route path="/genre">
-      <Genre />
-    </Route>
-    <Route path="/wishlist">
-      <Wishlist />
-    </Route>
-
       </div>
-    </Router>
+
   )
 }
 export default Navbar
