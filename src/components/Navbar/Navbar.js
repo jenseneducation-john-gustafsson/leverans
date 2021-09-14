131ae80e617d30c7180242b194380fa43cf1e2dd
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-09-14 11:32:08 +0200

    Add link to home/start page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import "./Navbar.css";
 import Wishlist from "../../view/Wishlist";
 import Bundle from "../Bundle/Bundle";
 import Genre from "../../view/CategoryOverview"
+import StartPage from "../../view/StartPage"
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
@@ -13,14 +14,17 @@ function Navbar() {
       <div>
         <nav>
           <div className="navbar">
-             <Link to=""><button className="navbar-buttons"><p className="nav-text">Top Rated</p></button></Link>
-            {/* <Link to=""><button className="navbar-buttons"><p className="nav-text">Latest</p></button></Link>  */}
+             {/* <Link to=""><button className="navbar-buttons"><p className="nav-text">Top Rated</p></button></Link> */}
+            <Link to="/"><button className="navbar-buttons"><p className="nav-text">Home</p></button></Link>
             <Link to="/bundles"><button className="navbar-buttons"><p className="nav-text">Bundles</p></button></Link>
             <Link to="/genre"><button className="navbar-buttons"><p className="nav-text">Genre</p></button></Link>
             <Link to="/wishlist"><button className="navbar-buttons"><p className="nav-text">Wishlist</p></button></Link>
           </div>
         </nav>
 
+    <Route path="/" exact>
+      <StartPage />
+    </Route>
     <Route path="/bundles">
       <Bundle />
     </Route>
