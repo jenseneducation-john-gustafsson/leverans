c830580e071d62458b4751470a865be61fd6a24e
tolli-txt <oskartolli@hotmail.com>
2021-09-15 14:43:17 +0200

    Added validation rules/messages, updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Header from "./components/Header/Header"
 import Navbar from "./components/Navbar/Navbar"
+import RegisterForm from "./view/RegisterForm"
+import messages from "./components/Forms/validation-messages"
+import rules from "./components/Forms/validation-rules"
 
 
 function App() {
@@ -8,7 +11,10 @@ function App() {
     <div className="App">
       <Header />
       <Navbar />
-      
+      <FormProvider rules={rules} messages={messages}>
+        <RegisterForm />
+      </FormProvider>
+
     </div>
   );
 }
