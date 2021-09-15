71efaf15650758c267f2a0ca9c9cbe8a38c31b0a
tolli-txt <oskartolli@hotmail.com>
2021-09-15 16:29:10 +0200

    Added fetch & changed into functional component

--- src/view/RegisterForm.js
+++ src/view/RegisterForm.js
@@ -1,41 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form } from 'react-advanced-form'
 import { Input, Button } from 'react-advanced-form-addons'
 
-export default class RegisterForm extends React.Component {
-  registerUser = ({ serialized }) => {
-    alert(JSON.stringify(serialized, null, 2))
+const RegisterForm = () => {
 
-    /* Perform async request with the serialized data */
-    return new Promise((resolve) => resolve())
+  const [inputEmail, setInputEmail] = useState('')
+  const [inputPassword, setInputPassword] = useState('')
+
+  const registerUser = ({ serialized }) => {
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: inputEmail, password: inputPassword })
+    };
+
+    fetch('http://localhost:3000/signup', requestOptions)
+      .then(response => response.json())
+      .then(data => this.setState({ postId: data.id }));
+
+    console.log({ email: inputEmail, password: inputPassword })
   }
 
-  render() {
-    return (
-      <Form action={this.registerUser}>
-        <Input
-          name="userEmail"
-          type="email"
-          label="Email"
-          required
-        />
-        <Input
-          name="userPassword"
-          type="password"
-          label="Password"
-          required
-        />
-        <Input
-          name="confirmPassword"
-          type="password"
-          label="Confirm password"
-          required
-          skip
-        />
+  return (
+    <Form action={registerUser}>
+      <Input
+        name="userEmail"
+        type="email"
+        label="Email"
+        onChange={(event) => { setInputEmail(event) }}
+        required
+      />
+      <Input
+        name="userPassword"
+        type="password"
+        label="Password"
+        onChange={(event) => { setInputPassword(event) }}
+        required
+      />
+      <Input
+        name="confirmPassword"
+        type="password"
+        label="Confirm password"
+        required
+        skip
+      />
 
-        <Button primary>Register</Button>
-        <p>Already have an account? Login <a href="#">here</a>!</p>
-      </Form>
-    )
-  }
+      <Button primary>Register</Button>
+      <p>Already have an account? Login <a href="#">here</a>!</p>
+    </Form>
+  )
 }
+
+export default RegisterForm
