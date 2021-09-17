4b2ed14e1339ddb42b3487475cdd1cef3d281fdd
divcoder20 <danny_tesfazion@hotmail.com>
2021-09-17 23:56:24 +0200

    added mongodb connection options

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,12 @@ const mongoose = require("mongoose");
 const ConnectDB = async () => {
   try {
 
-    await mongoose.connect("mongodb://localhost:27017/notflix_users?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false");
+    await mongoose.connect("mongodb://localhost:27017/notflix_users?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false", {
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log("MongoDB Connection Succeded!");
 
   } catch (error) {
