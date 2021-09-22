8e70c5632f4a3d70dd99749bc684e9debfd1a022
Nexuz49 <58955897+Nexuz49@users.noreply.github.com>
2021-09-22 10:45:09 +0200

    Update ModalBody.js

--- src/modules/modals/components/ModalBody.js
+++ src/modules/modals/components/ModalBody.js
@@ -2,6 +2,11 @@ export default function ModalBody(props) {
     return (
       <div className="modal-body">
         { props.children }
+        <iframe id="Geeks3" width="450" height="350"
+                                    src=
+ "https://www.youtube.com/embed/V5he1JXiQbg"
+                                    frameborder="0" allowfullscreen>
+                            </iframe>
       </div>
     );
   }
