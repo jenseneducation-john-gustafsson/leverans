84a20904a607b0e6b79aaf88dc0cbb8af38248eb
Josef Hedblom <hedblomjosef@gmail.com>
2021-09-26 20:21:45 +0200

    Update CategoryDetails (Action,Comedy and Horror) to fetch data from database.

--- src/components/Category-page/Category-details/CategoryDetailsHorror.js
+++ src/components/Category-page/Category-details/CategoryDetailsHorror.js
@@ -1,19 +1,32 @@
-/*css*/
+import React from 'react'
+import { useState, useEffect } from 'react'
 import "../Category-details/Category-details.css";
-import Products from '../../../products_test.json';
 import { Link } from "react-router-dom";
 
 import GetMovieDetails from '../Category-details/GetMovieDetails'
 function CategoryDetailsHorror() {
+
+  const [content, setContent] = useState([]);
+
+  async function fetchMoviesDetails() {
+    const MOVIE_DB = `/api/films`
+    const response = await fetch(MOVIE_DB);
+    const responseData = await response.json();
+    setContent(responseData);
+  }
+
+  useEffect(() => {
+    fetchMoviesDetails();
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps 
   return (
     <>
       <div className="container-fluid">
         <h2>Horror</h2> <Link to="/genre"><button className="Catbutton"><p className="nav-text">Return to Categories</p></button></Link>
         <div className="row2">
           {
-            Products.map(({ film }) => {
-              if (film.gener === "Horror") {
-                return <GetMovieDetails key={film.id} id={film.id} />
+            content.map((film) => {
+              if (film.genre === "Horror") {
+                return <GetMovieDetails key={film.apiId} id={film.apiId} />
               }
             })
           }
