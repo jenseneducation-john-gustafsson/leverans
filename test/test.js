3ef75610d2f98bcdc302eba4c858848e12f85cb9
Nexuz49 <58955897+Nexuz49@users.noreply.github.com>
2021-09-27 22:52:04 +0200

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,21 @@
+// let convert = require('../app.js') // Specify function location. 
+
+// require the built-in "assertion" library
+let assert = require('assert');
+
+// create a group of tests (ShoppingCart)
+describe('ShoppingCart General', function() {
+    // Nested/underlying Groups of tests
+    describe('functionNameForTestGoesHere', function() {
+        // A string explanation of what we are testing
+        it('StringExplanationGoesHere', function() {
+            // Actual test / Code/Logic Goes here
+            assert.equal()
+        });
+    });
+});
+
+
+
+// For tomorrow, Find and Examine Cart-Logic, then Write tests acording.
+// Functions in Cart.js ?.
