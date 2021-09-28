3ff658d65d4ffab9be3542b680b3363fcbfb56af
Josef Hedblom <hedblomjosef@gmail.com>
2021-09-28 13:35:55 +0200

    Create new files (BundleDetails(Action, Horror, Comedy and Animation)

---
+++ src/components/Bundle/BundleDetailsAnimation.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import "../Category-details/Category-details.css";
+import GetBundleDetails from './GetBundleDetails'
+
+function BundleDetailsAnimation() {
+
+  const [content, setContent] = useState([]);
+
+  async function fetchMoviesDetails() {
+    const MOVIE_DB = `/api/bundles`
+    const response = await fetch(MOVIE_DB);
+    const responseData = await response.json();
+    setContent(responseData);
+  }
+
+  useEffect(() => {
+    fetchMoviesDetails();
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps 
+  return (
+    <>
+      <div className="container-fluid">
+        <h2>Animation</h2>
+        <div className="row2">
+          {
+            content.map((bundle) => {
+              if (bundle.genre === "Animation") {
+                return <GetBundleDetails key={bundle.id} id={bundle.id} title={bundle.name} price={bundle.price} img={bundle.img} />
+              }
+            })
+          }
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default BundleDetailsAnimation;
--- src/components/Bundle/GetBundleOverview.js
+++ src/components/Bundle/GetBundleOverview.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import GetBundlesDetails from '../Bundle/GetBundleDetails'
 
-export default function GetBundleOverView(start,end) {
+export default function GetBundleOverView({ start, end }) {
 
   const [content, setContent] = useState([]);
 
